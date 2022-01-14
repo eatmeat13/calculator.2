88757551eda5516f9609b3e824a4185b57594668
Myasoedov <myasoedov.leon@mail.ru>
2022-01-14 19:15:39 +0300

    commit2

---
+++ src/solution.java
@@ -0,0 +1,66 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class solution {
+
+
+    static Scanner scanner = new Scanner(System.in);
+    static int number1, number2;
+    static char operation;
+    static int result;
+
+    public static void main(String[] args) {
+        System.out.println("Введите выражение ");
+        String userInput = scanner.nextLine();
+        char[] under_char = new char[5];
+        for (int i = 0; i < userInput.length(); i++) {
+            under_char[i] = userInput.charAt(i);
+            if (under_char[i] == '+') {
+                operation = '+';
+            }
+            if (under_char[i] == '-') {
+                operation = '-';
+            }
+            if (under_char[i] == '*') {
+                operation = '*';
+            }
+            if (under_char[i] == '/') {
+                operation = '/';
+            }
+        }
+        String under_charString = String.valueOf(under_char);
+        String[] blacks = under_charString.split("[+-/*]");
+        String stable00 = blacks[0].trim();
+        String stable01 = blacks[1].trim();
+        number1 = voll.romanToNumber(stable00);
+        number2 = voll.romanToNumber(stable01);
+
+        if (number1 < 0 && number2 < 0) {
+            number1 = Integer.parseInt(stable00);
+            number2 = Integer.parseInt(stable01);
+            if (operation == '/' && number2 ==0 ) {
+                System.out.println("Ошибка, деление на ноль");
+                return;
+            }
+            result = voll.calculated(number1, number2, operation);
+            System.out.println("Результат арабских цифр:");
+            System.out.print(result);
+        } else {
+
+            if (number1 == -1 || number2 == -1) {
+                throw new RuntimeException("Используются одновременно разные системы исчисления");
+            }
+            result = voll.calculated(number1, number2, operation);
+            System.out.println("Результат для римских цифр:");
+            if (result < 0) {
+                throw new RuntimeException("Римские числа не могут быть отрицательными");
+
+
+            }
+            String resultRoman = voll.convertNumToRoman(result);
+
+            System.out.println(resultRoman);
+
+        }
+    }
+}
---
+++ src/voll.java
@@ -0,0 +1,72 @@
+import java.util.InputMismatchException;
+
+public class voll {
+    static String convertNumToRoman(int numArabian)  {
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
+        final String ss = roman[numArabian];
+        return ss;
+    }
+
+
+    static int romanToNumber(String roman) {
+        try {
+            switch (roman) {
+                case "I":
+                    return 1;
+                case "II":
+                    return 2;
+                case "III":
+                    return 3;
+                case "IV":
+                    return 4;
+                case "V":
+                    return 5;
+                case "VI":
+                    return 6;
+                case "VII":
+                    return 7;
+                case "VIII":
+                    return 8;
+                case "IX":
+                    return 9;
+                case "X":
+                    return 10;
+            }
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Неверный формат данных");
+        }
+        return -1;
+    }
+
+    public static int calculated(int num1, int num2, char op)  {
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                try {
+                    result = num1 / num2;
+                    break;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+        }
+        return result;
+    }
+}
